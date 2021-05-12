8953e8dccb76a0370beea390629142ded9feb5c0
Christo-IV <christo.vahtramae@khk.ee>
2021-05-12 09:26:27 +0300

    Added a new test which adds 3 and deletes 2

--- cypress/integration/todo-list.spec.js
+++ cypress/integration/todo-list.spec.js
@@ -18,5 +18,41 @@ context('Todo List', () => {
             })
         })
     });
+
+    it('Add 3 and remove 2', () => {
+        cy.get('input')
+            .type('Tres').should('have.value', 'Tres')
+
+        cy.get('form').find('button').click().then(() => {
+            cy.get('input').should('have.value', '')
+            cy.get('ul').find('li').should('have.length', 1)
+        })
+
+        cy.get('input')
+            .type('Dos').should('have.value', 'Dos')
+
+        cy.get('form').find('button').click().then(() => {
+            cy.get('input').should('have.value', '')
+            cy.get('ul').find('li').should('have.length', 2)
+        })
+
+        cy.get('input')
+            .type('Uno').should('have.value', 'Uno')
+
+        cy.get('form').find('button').click().then(() => {
+            cy.get('input').should('have.value', '')
+            cy.get('ul').find('li').should('have.length', 3)
+        })
+
+        // Deletion
+
+        cy.get('li:nth-child(2)').find('button').click().then(() => {
+            cy.get('ul').find('li').should('have.length', 2)
+        })
+
+        cy.get('li:nth-child(2)').find('button').click().then(() => {
+            cy.get('ul').find('li').should('have.length', 1)
+        })
+    });
 })
   
